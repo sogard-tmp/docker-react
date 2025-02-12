8c26f5df163d11eebb1d1f57114806de1a021ef8
sogard-tmp <sogard.world@gmail.com>
2025-02-12 10:12:43 +0200

    bla bla

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          this was changed.
+          I am changing again.
         </p>
         <a
           className="App-link"
